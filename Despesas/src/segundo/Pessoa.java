8d042aaba33a7b9a9f2d22dfbd4137eb8fc39df5
Ronaldo Pereira <ox99709179@gmail.com>
2017-09-19 12:22:52 -0300

    adicionado comentários

--- Despesas/src/segundo/Pessoa.java
+++ Despesas/src/segundo/Pessoa.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Scanner;
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+//import java.util.Scanner;
 
 public class Pessoa {
 	
