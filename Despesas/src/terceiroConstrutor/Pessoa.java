d4a95a10dc8716cb7ebb39a0f569948bfcbdedc4
Ronaldo Pereira <ox99709179@gmail.com>
2017-10-05 17:54:29 -0300

    teste com construtor

---
+++ Despesas/src/terceiroConstrutor/Pessoa.java
@@ -0,0 +1,13 @@
+package terceiroConstrutor;
+
+public class Pessoa {
+	public String nome ;
+	public String email;
+
+	public Pessoa(String nome, String email){
+		
+		this.nome = 	nome;
+		this.email = 	email;
+	}
+
+}
---
+++ Despesas/src/terceiroConstrutor/Teste.java
@@ -0,0 +1,14 @@
+package terceiroConstrutor;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+//		Pessoa p = new Pessoa("escobar", "email");
+//		Pessoa p = new Pessoa("ronaldo","email.com");
+		
+		System.out.println(p.nome);
+
+	}
+
+}
